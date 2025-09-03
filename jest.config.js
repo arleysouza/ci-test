bfcf1cf03aa376dcfac4214f20d4b734bd275805
arleysouza <arley.ferreira.souza@gmail.com>
2025-09-03 19:28:53 -0300

    jeste.config.j

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,12 @@
-import type { Config } from "jest";
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.ts"],
   moduleFileExtensions: ["ts", "js", "json"],
   testMatch: ["**/tests/**/*.test.ts"],
   verbose: true,
-
-   // Adicione estas linhas para resolver o warning de ES module
   transform: {
-    "^.+\\.ts$": "ts-jest"
+    "^.+\\.ts$": "ts-jest",
   },
-  extensionsToTreatAsEsm: [".ts"]
 };
-
-export default config;
